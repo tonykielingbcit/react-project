949efbae90209e79babeb3e5abe172a36a068b25
Tony Kieling <tony.kieling@gmail.com>
2022-12-09 12:57:03 -0800

    .env-example added

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,22 +11,26 @@ const Home = () => {
             const getMovies = await fetch
                 (`https://api.themoviedb.org/3/movie/popular?api_key=${env.TMDB_KEY}&language=en-US&page=1`)
                     .then((response) => response.json());
-
+                    
             setTitles(getMovies.results);
         };
-console.log("TMDB_KEY====== ", env.TMDB_KEY);
+        
         fetchData();
     }, []);
 
 
     const MoviesList = () => {
         let temp = [];
+        
         if (titles && titles.length > 0)
             temp = titles.map((e, i) => {
                 return (
                     <div key={i} style={{border: "1px solid blue", margin: "1rem 0"}}>
                         <p>Id: { e.id }</p>
                         <p>Name: { e.title }</p>
+                        <figure>
+                            <img src = {`https://image.tmdb.org/t/p/w300/${e.poster_path}`} alt={e.title} />
+                        </figure>
                     </div>
                 );
             });
@@ -37,10 +41,9 @@ console.log("TMDB_KEY====== ", env.TMDB_KEY);
     return(
         <div>
             <p>Hello and welcome for the Home page!</p>
-            {console.log("titles:: ", titles)}
     
             {
-                titles && (titles.length > 0) && <MoviesList />
+                <MoviesList />
             }
         </div>
     );
